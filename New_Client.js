2b74b76555c30cad0ad8e5682deff0ada8505d14
Jrids56 <jrids56@gmail.com>
2022-12-23 19:48:47 +1100

    Copy From Google API Example

---
+++ New_Client.js
@@ -0,0 +1,119 @@
+// This sample uses the Places Autocomplete widget to:
+// 1. Help the user select a place
+// 2. Retrieve the address components associated with that place
+// 3. Populate the form fields with those address components.
+// This sample requires the Places library, Maps JavaScript API.
+// Include the libraries=places parameter when you first load the API.
+// For example: <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let autocomplete;
+let address1Field;
+let address2Field;
+let postalField;
+
+function initAutocomplete() {
+  address1Field = document.querySelector("#ship-address");
+  address2Field = document.querySelector("#address2");
+  postalField = document.querySelector("#postcode");
+
+  autocomplete = new google.maps.places.Autocomplete(address1Field, {
+    // restrict country search to australia
+    componentRestrictions: { country: ["aus"] },
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+  });
+  address1Field.focus();
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  const place = autocomplete.getPlace();
+  let address1 = "";
+  let postcode = "";
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  // place.address_components are google.maps.GeocoderAddressComponent objects
+  // which are documented at http://goo.gle/3l5i5Mr
+  for (const component of place.address_components) {
+    // @ts-ignore remove once typings fixed
+    const componentType = component.types[0];
+
+    switch (componentType) {
+      case "street_number": {
+        address1 = `${component.long_name} ${address1}`;
+        break;
+      }
+
+      case "route": {
+        address1 += component.short_name;
+        break;
+      }
+
+      case "postal_code": {
+        postcode = `${component.long_name}${postcode}`;
+        break;
+      }
+
+      case "postal_code_suffix": {
+        postcode = `${postcode}-${component.long_name}`;
+        break;
+      }
+      case "locality":
+        document.querySelector("#locality").value = component.long_name;
+        break;
+      case "administrative_area_level_1": {
+        document.querySelector("#state").value = component.short_name;
+        break;
+      }
+      case "country":
+        document.querySelector("#country").value = component.long_name;
+        break;
+    }
+  }
+
+  address1Field.value = address1;
+  postalField.value = postcode;
+  // After filling the form with address components from the Autocomplete
+  // prediction, set cursor focus on the second address line to encourage
+  // entry of subpremise information such as apartment, unit, or floor number.
+  address2Field.focus();
+}
+
+window.initAutocomplete = initAutocomplete;
+
+// Create a function which stores the address in the database on the table and saves when the page is refreshed
+function saveAddress() {
+    // Get the value of the address field
+    let address = document.getElementById("ship-address").value;
+    // Get the value of the address2 field
+    let address2 = document.getElementById("address2").value;
+    // Get the value of the locality field
+    let locality = document.getElementById("locality").value;
+    // Get the value of the state field
+    let state = document.getElementById("state").value;
+    // Get the value of the postcode field
+    let postcode = document.getElementById("postcode").value;
+    // Get the value of the country field
+    let country = document.getElementById("country").value;
+    
+    // Save the address in the database
+    $.post(
+        "save_address.php",
+        {
+        address: address,
+        address2: address2,
+        locality: locality,
+        state: state,
+        postcode: postcode,
+        country: country,
+        },
+        function (data, status) {
+        // Reload the page
+        location.reload();
+        }
+    );
+    }
